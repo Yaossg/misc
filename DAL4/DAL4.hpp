179287b0fb5fc11769ef1753c88a0057dda35104
Yaosausage <yaossg@outlook.com>
2021-10-09 21:32:49 +0800

    -

--- DAL4/DAL4.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef DAL_4_HPP
-#define DAL_4_HPP
-
-#include "mean_reflector.hpp"
-#include "variance.hpp"
-#include "mode.hpp"
-#include "median.hpp"
-
-#endif
--- expression/gcds.cpp
+++
@@ -1,9 +0,0 @@
-int gcd1(int a, int b) {
-	while(a%=b^=a^=b);
-	return b;
-}
-
-int gcd2(int a, int b) {
-	while(b%=a-=b=(a+=b)-b);
-	return a;
-}
